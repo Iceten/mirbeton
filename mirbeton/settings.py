d60f4f530ea5c510a5bff0054e23a6531d95288e
idriss <idrissmouliom@gmail.com>
2023-04-09 16:23:16 +0200

    Removing ...

--- mirbeton/settings.py
+++ mirbeton/settings.py
@@ -45,8 +45,8 @@ STATIC_DIR = os.path.join(BASE_DIR, "static")
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4w*g6afvbs=ae^4%z&m%i^5qt3fl2gn)_o0z9)h6p#1!0l0#)e'
 
+SECRET_KEY = env.str('SECRET_KEY', 'sample_secret')
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'mirbeton.com', 'mirbeton.fr']
 
 
